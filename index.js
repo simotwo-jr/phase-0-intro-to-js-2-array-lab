4dbf19a8cf3b2805427296201c4052ad764b1aa0
Nehemiah Cheruiyot <nehemiahcheruiyot7069@gmail.com>
2023-12-16 21:47:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Write your solution here!
+// index.js
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+// other functions and exports as needed
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+};
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  // other functions as needed
+  // index.js
+
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
+
+
+
